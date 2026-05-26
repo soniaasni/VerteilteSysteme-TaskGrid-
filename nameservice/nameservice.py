5c76070eb3ab7035616ec97c0d0303a40a80cf4a
Xplosion <sommerwinter05@gmail.com>
2026-05-26 10:32:04 +0200

    Update nameservice.py

--- nameservice/nameservice.py
+++ nameservice/nameservice.py
@@ -1,23 +1,52 @@
 class Namensdienst:
-    def __init__(self,other_args):
-        self.other_args = other_args
+    def __init__(self):
+        self.workers = []
+        self.idcount = 0
 
-    register_worker(const char* type, const char* address)
-lookup_worker(const char* type)
-deregister_worker(const char* address)
-receive_heartbeat(const char* worker_id)
+    def register_worker(self,type,address,port):
+        self.workers.append(Worker(type,address,port,self.idcount,self))
+        self.idcount += 1
+        print("Registered Worker of type " + type + " with address " + address + ":" + port)
+        return True
+
+    def lookup_worker(self,type):
+        result = []
+        for worker in self.workers:
+            if worker.type==type:
+                result.append(worker)
+        print("Found workers of type " + type + ": ")
+        for worker in result:
+            print("Worker " + str(worker.id) + ": "+ worker.address + ", " + worker.port)
+        return result
+        
+
+    def deregister_worker(self,address,port):
+        for worker in self.workers:
+            if worker.address == address and worker.port == port:
+                self.workers.remove(worker)
+                print("De-Registered Worker with address " + address + ":" + port)
+                return True
+        print("Could not find Worker with address " + address + " and port " + port)
+        return False
+    
+    def receive_heartbeat(worker_id):
+        a = 1
 
 class Worker:
-    def __init__(self,type,address,port,dienst):
+    def __init__(self,type,address,port,id,dienst):
         self.type = type
         self.address = address
         self.port = port
+        self.id = id
         self.dienst = dienst
-    
-    def getID(self):
-        self.worker_id = self.dienst.get
+        
+testDienst = Namensdienst()
+testDienst.register_worker("flame_grill","google.com","98765")
+testDienst.register_worker("flame_grill","amazon.com","12345")
+testDienst.lookup_worker("flame_grill")
+testDienst.deregister_worker("google.com","98765")
 
-
+"""
 status
 last_heartbeat
 current_load
@@ -26,3 +55,6 @@ ACTIVE
 UNHEALTHY
 DRAINING
 OFFLINE
+
+For communication gRPC
+"""
